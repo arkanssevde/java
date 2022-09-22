9e13f9194eff321e1fd3370e3680f547336a8118
sevdegül arkan <sevdegl arkan@sevde>
2022-09-22 12:22:17 +0300

    java ilk

---
+++ data/src/data/main.java
@@ -0,0 +1,15 @@
+package data;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		String[] krediler = {"hızlı krediler","mutlu emekli"};
+        System.out.println(krediler[0]);
+        for (int i = 0; i < krediler.length; i++) {
+			System.out.println(krediler[i]);
+			
+	}
+
+}
+}
